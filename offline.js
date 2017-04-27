3b4970d310491b930995dc23ee85a5d4635b4a8c
Paolo Diego de Souza <p4olodiego@gmail.com>
2017-04-27 01:46:45 -0300

    test1
    test1

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1493260299,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/tiledbackground.png",
+		"images/char_anim-sheet0.png",
+		"images/platforms1-sheet0.png",
+		"images/charhitbox-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
